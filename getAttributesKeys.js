c85158438cfc42d1083ecbabc1fdea9c230e1f27
Pei Hu <hupei1991@gmail.com>
2018-03-05 16:09:47 -0500

    Update getAttributesKeys.js

--- getAttributesKeys.js
+++ getAttributesKeys.js
@@ -1,10 +1,8 @@
 // get all attributes keys for UMLS NLP JSON files
 
-var fs = require('fs');
+ var fs = require('fs');
 
- const resultSet = new Set();
-
- function readFiles(dirname, onFileContent, onError) {
+ function readFiles(dirname, onFileContent, onError, resultSet) {
     console.log("Started to parsing files in directory: " + dirname);
     fs.readdir(dirname, function(err, filenames) {
         if (err) {
@@ -15,7 +13,7 @@ var fs = require('fs');
             fs.readFile(dirname + filename, 'utf-8', function(err, content) {
                 if (fs.lstatSync(dirname + filename).isFile()) {
                     if (filename.endsWith("done")) {
-                        onFileContent(filename, content);
+                        onFileContent(filename, content, resultSet);
                     }
                     if (err) {
                         onError(err);
@@ -27,19 +25,29 @@ var fs = require('fs');
     })
  }
 
- function onFileContent(filename, content) {
-    console.log("parsing " + filename + "...");
+ function onFileContent(filename, content, resultSet) {
+    // console.log("parsing " + filename + "...");
     var doc = JSON.parse(content);
     var resultList = doc.result;
     Object.keys(resultList).forEach(function(key) {
         var attributes = resultList[key].attributes;
         for (var k in attributes) {
+            if (!resultSet.has(k)) {
+                console.log(k);
+            }
             resultSet.add(k);
         }
     });
-    console.log(resultSet);
  }
 
- readFiles("./", onFileContent, function(err) {
-     throw err;
- });
+
+
+ if (require.main === module) { 
+
+    const resultSet = new Set();
+
+    readFiles("/home/phu/JsonData/DATA IMAGES TO CONVERT ORIGINAL OCR TEXT TO NLP FHIR JSON/", onFileContent, function(err) {
+        throw err;
+    }, resultSet);
+
+}
